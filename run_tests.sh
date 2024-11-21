cc1e4b57b792a6d8562d55148fe0923b48fcdd42
Marcus Aandahl <marcus.aandahl@gmail.com>
2024-11-21 11:00:14 +0100

    Remove tests 1-4

--- run_tests.sh
+++ run_tests.sh
@@ -13,29 +13,6 @@ echo -e "${LG}MAKE...${NC}"
 make
 clear
 
-#echo -e "${LG}RUNNING TEST 01${NC}"
-#../pxedrive24/pxy/pxydrive.py -f ../pxedrive24/s01*.cmd -S 3 -p ./proxy
-#read -p "Press any key to continue to next test... " -n1 -s
-#clear
-#
-#
-#echo -e "${LG}RUNNING TEST 02${NC}"
-#../pxedrive24/pxy/pxydrive.py -f ../pxedrive24/s02*.cmd -S 3 -p ./proxy
-#read -p "Press any key to continue to next test... " -n1 -s
-#clear
-#
-#
-#echo -e "${LG}RUNNING TEST 03${NC}"
-#../pxedrive24/pxy/pxydrive.py -f ../pxedrive24/s03*.cmd -S 3 -p ./proxy
-#read -p "Press any key to continue to next test... " -n1 -s
-#clear
-#
-#
-#echo -e "${LG}RUNNING TEST 04${NC}"
-#../pxedrive24/pxy/pxydrive.py -f ../pxedrive24/s04*.cmd -S 3 -p ./proxy
-#read -p "Press any key to continue to next test... " -n1 -s
-#clear
-
 
 echo -e "${LG}RUNNING TEST 05${NC}"
 ../pxedrive24/pxy/pxydrive.py -f ../pxedrive24/s05*.cmd -S 3 -p ./proxy
